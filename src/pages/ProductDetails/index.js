de06396381877876ed34bde7af8e07cb488fd5f2
giang <vuthithaogiang050400@gmail.com>
2023-10-21 12:48:47 +0700

    Details product - Part 3: add Suggest items + dark mode

--- src/pages/ProductDetails/index.js
+++ src/pages/ProductDetails/index.js
@@ -3,6 +3,7 @@ import styles from './ProductDetails.module.scss';
 import images from '~/assets/images';
 import BackToTop from '~/components/BackToTop';
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const cx = classNames.bind(styles);
 
@@ -29,7 +30,106 @@ function ProductDetails() {
         ],
     };
 
+    const PRODUCTS_SUGGEST = [
+        {
+            id: 6,
+            brand: 'sddsfdg',
+            thumbnail:
+                'https://images.squarespace-cdn.com/content/v1/5cb9ef147eb88c5caefa30b3/1658910220043-YPFMH9RGY6YXQMG5XIU1/75+In+Summer%2C+I+Cook+What+My+Garden+Gives+Me+20.jpg?format=1500w',
+
+            name: 'Raindrop Cake',
+            price: 21,
+            saved: false,
+            rating: 5,
+        },
+        {
+            id: 7,
+
+            thumbnail:
+                'https://images.squarespace-cdn.com/content/v1/5cb9ef147eb88c5caefa30b3/1689264374858-OXQEW3UTZ48OPOFXAB15/Summer+Cooking+-+Her86m2_1.117.1.jpg?format=1500w',
+
+            name: 'Bubble Tea',
+            price: 50,
+            brand: 'Wonder Bread',
+            rating: 3.8,
+            saved: false,
+        },
+        {
+            id: 8,
+
+            thumbnail:
+                'https://images.squarespace-cdn.com/content/v1/5cb9ef147eb88c5caefa30b3/1670249967692-5ZKGHBUHXNAHK2VYGLVX/Vegetable+Garden+2022+-+Her86m2+-+413.jpg?format=1500w',
+
+            name: 'Tomato',
+            price: 20,
+            brand: 'Fresh Garden',
+            reting: 4.2,
+            saved: true,
+        },
+        {
+            id: 9,
+
+            thumbnail:
+                'https://images.squarespace-cdn.com/content/v1/5cb9ef147eb88c5caefa30b3/1670249665894-9VZY77JHHC9EKVQJX383/Everything+About+Growing+Tomatoes+-+Her86m2+_1.177.1.jpg?format=1500w',
+
+            name: 'Spanish',
+            price: 20,
+            brand: 'Fresh Garden',
+            rating: 4.5,
+            saved: false,
+        },
+        {
+            id: 10,
+            thumbnail:
+                'https://images.squarespace-cdn.com/content/v1/5cb9ef147eb88c5caefa30b3/1624481236729-MNKBXLJCV6JN22OTXV1Y/6+Strawberry+Recipes+for+Hot+Summer+-+Her+86m2+_R3O0936.JPG?format=1500w',
+
+            name: 'Strawberries Sweet',
+            price: 30,
+            brand: 'Fresh Garden',
+            rating: 3.9,
+            saved: false,
+        },
+        {
+            id: 11,
+
+            thumbnail:
+                'https://images.squarespace-cdn.com/content/v1/5cb9ef147eb88c5caefa30b3/1660752377787-N1L0S4T4OU9BKKURCFUJ/_R3O5168-2.jpg?format=1500w',
+
+            name: 'Leaf Green',
+            price: 30,
+            brand: 'Fresh Garden',
+            rating: 5,
+            saved: false,
+        },
+        {
+            id: 12,
+
+            thumbnail:
+                'https://images.squarespace-cdn.com/content/v1/5cb9ef147eb88c5caefa30b3/1681323739922-WL5L12RJ42FWY92GZDAJ/Small+Bites+Brunch+Buffet+Ideas+for+your+next+party+-+Her86m2+_1.171.1.jpg?format=1500w',
+
+            name: 'Bites Brunch',
+            price: 34,
+            brand: 'Brunches',
+            rating: 4.7,
+            saved: true,
+        },
+        {
+            id: 13,
+
+            thumbnail:
+                'https://images.squarespace-cdn.com/content/v1/5cb9ef147eb88c5caefa30b3/1689258526521-DKKEQPO5IZG4ZJD6VG1L/Summer+Cooking+-+Her86m2+41.jpg?format=1500w',
+
+            name: 'Lemon Curd Cookies',
+            price: 20,
+            brand: 'Curd Cookies',
+            rating: 4,
+            saved: false,
+        },
+    ];
+
     const [productTab, setProductTab] = useState('Similar');
+
+    const navigate = useNavigate();
 
     return (
         <div className={cx('wrapper')}>
@@ -47,7 +147,7 @@ function ProductDetails() {
                 </header>
 
                 <div className={cx('product-desc')}>
-                    <div className={cx('row')}>
+                    <div className="row">
                         <div className={cx('col-5', 'left')}>
                             <div className={cx('product-preview')}>
                                 <div className={cx('product-preview-list')}>
@@ -80,7 +180,7 @@ function ProductDetails() {
                             <form method="post" onSubmit={(e) => e.preventDefault()}>
                                 <section className={cx('product-info')}>
                                     <h1 className={cx('product-info-heading')}>{PRODUC_DATA.name}</h1>
-                                    <div className={cx('row')}>
+                                    <div className="row">
                                         <div className={cx('col-6', 'left-product-info')}>
                                             <div className={cx('product-property')}>
                                                 <img
@@ -217,7 +317,7 @@ function ProductDetails() {
                                 <>
                                     <div className={cx('product-tab-content')}>
                                         <h2 className={cx('product-content-heading')}>What our customers are saying</h2>
-                                        <div className={cx('row', 'row-cols-3')}>
+                                        <div className="row row-cols-3">
                                             {/* column 1 */}
                                             <div className={cx('col')}>
                                                 <div className={cx('review-card')}>
@@ -369,7 +469,57 @@ function ProductDetails() {
                                 </>
                             )}
                             {productTab === 'Similar' && (
-                                <div className={cx('product-tab-content')}>Content Similar</div>
+                                <>
+                                    <div className={cx('product-tab-content')}>
+                                        <h2 className={cx('product-content-heading')}>
+                                            Similar items that you might like
+                                        </h2>
+
+                                        <div className={cx('list-products-suggest')}>
+                                            {PRODUCTS_SUGGEST.map((product) => (
+                                                <div className={cx('wrap-product')} key={product.id}>
+                                                    <figure className={cx('thumb')}>
+                                                        <img
+                                                            onClick={() => navigate(`/product-detail/${product.name}`)}
+                                                            className={cx('thumb-img')}
+                                                            alt=""
+                                                            src={product.thumbnail}
+                                                        />
+                                                        <div>
+                                                            {product.saved === false ? (
+                                                                <img
+                                                                    className={cx('icon')}
+                                                                    alt=""
+                                                                    src={images.heartIcon}
+                                                                />
+                                                            ) : (
+                                                                <img
+                                                                    className={cx('saved')}
+                                                                    alt=""
+                                                                    src={images.heartColor}
+                                                                />
+                                                            )}
+                                                        </div>
+                                                    </figure>
+                                                    <h3
+                                                        className={cx('name-product')}
+                                                        onClick={() => navigate(`/product-detail/${product.name}`)}
+                                                    >
+                                                        {[product.name]}
+                                                    </h3>
+                                                    <p className={cx('brand')}>{product.brand}</p>
+                                                    <div className={cx('row')}>
+                                                        <span>${product.price}</span>
+                                                        <span>
+                                                            <img className={cx('star')} alt="" src={images.starIcon} />
+                                                            {product.rating}
+                                                        </span>
+                                                    </div>
+                                                </div>
+                                            ))}
+                                        </div>
+                                    </div>
+                                </>
                             )}
                         </div>
                     </div>
